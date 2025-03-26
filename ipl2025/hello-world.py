eb9536abca202f67609028f2df1348348f796ed8
suddu16 <sudarshanrmail@gmail.com>
2025-03-25 23:51:24 -0700

    Update hello-world.py

--- ipl2025/hello-world.py
+++ ipl2025/hello-world.py
@@ -1,3 +1,3 @@
 print("hello")
 with open('./GitHub_Action_Results.txt', 'w') as f:
-    f.write(f"This was written with a GitHub action {driver.title}")
+    f.write(f"This was written with a GitHub action.")
